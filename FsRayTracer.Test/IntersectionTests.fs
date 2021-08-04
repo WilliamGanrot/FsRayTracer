dc0891d5f95c27addcaec3503d29fe35e0ec3395
William Ganrot <william.ganrot@youcal.com>
2021-08-04 22:28:03 +0200

    add intersection hit function

--- FsRayTracer.Test/IntersectionTests.fs
+++ FsRayTracer.Test/IntersectionTests.fs
@@ -26,7 +26,7 @@ let ``aggregating intersections`` () =
     let i1 = Intersection.create s 1.
     let i2 = Intersection.create s 2.
 
-    let xs = Intersection.intersections i1 i2
+    let xs = Intersection.intersections [i1; i2]
 
     xs.Length = 2 |> Assert.True
     xs.[0].t = 1. |> Assert.True
@@ -41,4 +41,47 @@ let ``intersect sets the object on the intersection`` () =
     xs.Length = 2 |> Assert.True
     xs.[0].object = s |> Assert.True
     xs.[1].object = s |> Assert.True
+
+[<Fact>]
+let ``the hit, when all intersections have positive t`` () =
+    let s = Shape.sphere
+    let i1 = Intersection.create s 1.
+    let i2 = Intersection.create s 2.
+    let xs = Intersection.intersections [i2; i1]
+
+    let i = xs |> Intersection.hit
+    i = Some (i1) |> Assert.True
+
+[<Fact>]
+let ``the hit, when some intersections have negative t`` () =
+    let s = Shape.sphere
+    let i1 = Intersection.create s -1.
+    let i2 = Intersection.create s 1.
+    let xs = Intersection.intersections [i2; i1]
+
+    let i = xs |> Intersection.hit
+    i = Some (i2) |> Assert.True
+
+[<Fact>]
+let ``the hit, when all intersections have negative t`` () =
+    let s = Shape.sphere
+    let i1 = Intersection.create s -2.
+    let i2 = Intersection.create s -1.
+    let xs = Intersection.intersections [i2; i1]
+
+    let i = xs |> Intersection.hit
+    i = None |> Assert.True
+
+
+[<Fact>]
+let ``the hit is always the lowest nonnegative intersection`` () =
+    let s = Shape.sphere
+    let i1 = Intersection.create s 5.
+    let i2 = Intersection.create s 7.
+    let i3 = Intersection.create s -3.
+    let i4 = Intersection.create s 2.
+    let xs = Intersection.intersections [i1; i2; i3; i4]
+
+    let i = xs |> Intersection.hit
+    i = Some(i4) |> Assert.True
 
--- FsRayTracer.Tracer/Intersection.fs
+++ FsRayTracer.Tracer/Intersection.fs
@@ -10,6 +10,15 @@ module Intersection =
     let create (s:Sphere) (t:float) =
         {object = s; t = t}
 
-    let intersections i1 i2 : Intersection list =
-        [i1;i2]
+    let intersections (l: Intersection list) : Intersection list =
+        l
 
+    let hit (l: Intersection list) : Option<Intersection>=
+        let hitsInfrontOfOrigin =
+            l
+            |> List.filter (fun i -> i.t > 0.)
+            |> List.sortBy (fun i -> i.t)
+
+        match hitsInfrontOfOrigin with
+        | [] -> None
+        | h::_ -> Some h

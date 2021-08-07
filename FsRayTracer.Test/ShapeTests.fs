53da80130c4f9e3873d9c1a08e1b7b666160fd9a
William Ganrot <william.ganrot@youcal.com>
2021-08-07 22:16:09 +0200

    calculating shape normal

--- FsRayTracer.Test/ShapeTests.fs
+++ FsRayTracer.Test/ShapeTests.fs
@@ -47,3 +47,54 @@ let ``intersecting a translated sphere with a ray`` () =
     let xs = Ray.intersect s2 r
 
     xs.Length = 0 |> Assert.True
+
+[<Fact>]
+let ``the normal on a sphere poin on the x axis`` () =
+    let p = Point.create 1. 0. 0.
+    let s = Shape.sphere
+    let n = s |> Shape.normal p
+
+    n .= (Vector.create 1. 0. 0.) |> Assert.True
+
+[<Fact>]
+let ``the normal on a sphere poin on the y axis`` () =
+    let p = Point.create 0. 1. 0.
+    let s = Shape.sphere
+    let n = s |> Shape.normal p
+
+    n .= (Vector.create 0. 1. 0.) |> Assert.True
+
+[<Fact>]
+let ``the normal on a sphere poin on the z axis`` () =
+    let p = Point.create 0. 0. 1.
+    let s = Shape.sphere
+    let n = s |> Shape.normal p
+
+    n .= (Vector.create 0. 0. 1.) |> Assert.True
+
+[<Fact>]
+let ``the normal is a normalized vector`` () =
+    let p = Point.create (Math.Pow (3., 0.5)/3.) (Math.Pow (3., 0.5)/3.) (Math.Pow (3., 0.5)/3.)
+    let s = Shape.sphere
+    let n = s |> Shape.normal p
+
+    n .= Vector.normalize (n) |> Assert.True
+
+[<Fact>]
+let ``cumputing the normal on a translated sphere`` () =
+    let n =
+        Shape.sphere
+        |> Shape.transform (Translation(0., 1., 0.))
+        |> Shape.normal (Point.create 0. 1.70711 -0.70711)
+
+    n .= (Vector.create 0. 0.70711 -0.70711) |>  Assert.True
+
+[<Fact>]
+let ``cumputing the normal on a transformed sphere`` () =
+    let n =
+        Shape.sphere
+        |> Shape.transform (Scaling(1., 0.5, 1.))
+        |> Shape.transform (Rotation(Z, (Math.PI/5.)))
+        |> Shape.normal (Point.create 0. (Math.Pow (2., 0.5)/2.) -(Math.Pow (2., 0.5)/2.))
+
+    n .= (Vector.create 0. 0.97014 -0.24254) |>  Assert.True
--- FsRayTracer.Tracer/Shape.fs
+++ FsRayTracer.Tracer/Shape.fs
@@ -3,6 +3,9 @@ namespace RayTracer.Shape
 open System
 open RayTracer.Transformation
 open RayTracer.Matrix
+open RayTracer.Vector
+open RayTracer.Point
+
 
 [<AutoOpen>]
 module Domain =
@@ -19,3 +22,19 @@ module Shape =
     let transform (t:Transformation) (s:Sphere) : Sphere =
         let t = Transformation.applyToMatrix t s.transform
         {s with transform = t}
+
+    let normal (p:Point) (s:Sphere) =
+
+        let objectPoint =
+            (Matrix.inverse s.transform)
+            |> Matrix.multiplyPoint p
+
+        let objectNormal = objectPoint - (Point.create 0. 0. 0.)
+
+        let worldNormal =
+            s.transform
+            |> Matrix.inverse
+            |> Matrix.Transpose
+            |> Matrix.multiplyVector objectNormal
+
+        Vector.normalize {worldNormal with W = 0.}
